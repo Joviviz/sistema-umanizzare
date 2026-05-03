9fbc730f953641a0fc03eb8f950f4f9e94112888
Joviviz <101208535+Joviviz@users.noreply.github.com>
2026-05-03 15:09:00 -0300

    Add initial Footer folder

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,14 @@ import './styles/global.css'
 import './styles/theme.css'
 
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 import { Header } from './components/Header'
+import { Footer } from './components/Footer';
+
 import { Home } from './pages/Home'
 import { Login } from './pages/Login'
 import { Register } from './pages/Register'
+
 
 
 
@@ -19,6 +23,7 @@ export function App() {
           <Route path='/login'    element={<Login />}/>
           <Route path='/register' element={<Register />}/>
         </Routes>
+        <Footer/>
       </BrowserRouter>
     </>
   );
--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,9 +1,9 @@
-import styles from './styles.module.css';
+import styles from "./styles.module.css";
 
 export function Footer() {
-  return (
-    <footer className={styles.footer}>
-      i am footer
-    </footer>
-  );
+    return(
+        <div className={styles.massa}>
+            <p >Eu sou o footer 1</p>
+        </div>
+    );
 }
